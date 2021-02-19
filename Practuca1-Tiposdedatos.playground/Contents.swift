84dc2ba660a61a0f9de5d132ace1de7e1fa45c35
Ma Guadalupe Brizuela <antares@MacBook-Air-de-Ma.local>
2021-02-18 20:13:55 -0600

    practica1

---
+++ Practuca1-Tiposdedatos.playground/Contents.swift
@@ -0,0 +1,78 @@
+import UIKit
+
+var mivariable = 1
+
+var str = "Hola Mundo"
+
+let PI = 3.1416
+
+
+var MiVariable = 2
+
+var miVariable = 3 //Metodo Camel Case
+
+print(MiVariable)
+
+print(miVariable)
+print (mivariable)
+
+var cadena = "Hola IOS"
+cadena = "swift"
+
+let nombre1:String = "Walter" //asignacion explicita
+var apellido1:String = "White"
+
+let saludo1:String = "Hola"
+
+//Concatenar cadenas con interpolaciòn se pone \
+print("HOLA \(nombre1) \(apellido1)")
+
+let nombre2:String = "Walter jr"
+let saludo2: String = "Hola \(nombre2)"
+
+print(saludo2)
+
+let CORREO = "billgates@microsoft.com"
+
+
+var entero = 1
+
+var a:Int = 10
+
+var b:Int = 20
+
+var suma:Int = a + b
+var resta:Int = a - b
+var mult:Int = a * b
+
+var x:Double = 10
+var y:Double = 10
+var pi:Double = x/y
+
+var radio:Double = 1.5
+
+var area:Double = pi * (radio * radio)
+
+var residuo = 30 % 4
+
+var  negativo = -40
+var positivo = +40
+
+positivo += 10
+
+var flotante = 1.5
+print(flotante)
+print("El valor de area es \(area)")
+
+var i:Int = 0 ; var j:Int = 0
+
+//Booleano
+
+var valorpositivo:Bool = true
+var valornegativo:Bool = false
+
+var p = 10
+var q = 10
+var comparacion:Bool = p == q
+print (comparacion)
+
