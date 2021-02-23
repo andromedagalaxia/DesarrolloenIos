f3a708a88f7b9cfe5ed622b9015f8ac821367d48
Ma Guadalupe Brizuela <antares@MacBook-Air-de-Ma.local>
2021-02-22 21:12:31 -0600

    Practica 3

---
+++ holaMundo/holaMundo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  holaMundo
+//
+//  Created by Ma Guadalupe Brizuela on 20/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
